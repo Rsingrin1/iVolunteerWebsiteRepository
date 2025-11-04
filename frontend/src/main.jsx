d24291716e0126d2aa09d62da2bb1de9130afa29
Rsingrin1 <rsingrin@csus.edu>
2025-11-03 17:45:53 -0800

    finished setting up dependencies

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,8 +7,10 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </StrictMode>,
 )
