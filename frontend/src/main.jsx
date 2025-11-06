22b2ec4dd906c4b280be80584d9710b702fe26fc
Rsingrin1 <rsingrin@csus.edu>
2025-11-06 15:12:21 -0800

    preparing to merge stuff

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { ChakraProvider} from "@chakra-ui/react"
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <StrictMode>
-    <App />
-  </StrictMode>
-  </BrowserRouter>
-,
+  <ChakraProvider>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </ChakraProvider>
 )
