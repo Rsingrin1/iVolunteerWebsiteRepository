1cb8d806f09c9c675e594e08ef1978025dc781f6
Rsingrin1 <rsingrin@csus.edu>
2025-11-06 17:59:47 -0800

    dani help :(

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { ChakraProvider} from "@chakra-ui/react"
+import { ChakraProvider } from '@chakra-ui/react'
 
 import App from './App.jsx'
 
