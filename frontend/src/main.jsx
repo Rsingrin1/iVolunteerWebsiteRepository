27836063e4d5807ac1c97377a1b3cf9ea6b5d77d
Rsingrin1 <rsingrin@csus.edu>
2025-11-06 18:26:45 -0800

    front end is 'working'

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,43 @@
-import { StrictMode } from 'react'
+/*
+import { React } from 'react/'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <ChakraProvider>
-    <BrowserRouter>
-      <StrictMode>
+  
+  <React.StrictMode>
+    <ChakraProvider value={defaultSystem}>
+      <BrowserRouter>
         <App />
-      </StrictMode>
     </BrowserRouter>
   </ChakraProvider>
+</React.StrictMode>
+
+
+  ) 
+*/
+  
+
+
+
+
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChakraProvider value={defaultSystem}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
 )
+
