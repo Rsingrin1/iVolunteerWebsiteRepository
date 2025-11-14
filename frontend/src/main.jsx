c4ddd6b34a6e8f8271d655381898ffbd758f783f
Rsingrin1 <rsingrin@csus.edu>
2025-11-13 18:45:29 -0800

    making the file structure for the front end

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,28 +1,3 @@
-/*
-import { React } from 'react/'
-import { createRoot } from 'react-dom/client'
-//import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
-
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  
-  <React.StrictMode>
-    <ChakraProvider value={defaultSystem}>
-      <BrowserRouter>
-        <App />
-    </BrowserRouter>
-  </ChakraProvider>
-</React.StrictMode>
-
-
-  ) 
-*/
-  
-
-
 
 
 import React from 'react'
@@ -30,6 +5,7 @@ import { createRoot } from 'react-dom/client'
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
