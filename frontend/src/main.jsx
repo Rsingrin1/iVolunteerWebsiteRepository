5fdbbb3046487c010654db2b76dae3fe01f6a4c9
Rsingrin1 <rsingrin@csus.edu>
2025-11-13 20:37:19 -0800

    moved the css files into the assets dir

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client'
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
 import App from './pages/App.jsx'
-import './index.css'
+import './assets/index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
