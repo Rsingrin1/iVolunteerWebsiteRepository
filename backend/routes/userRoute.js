16c758b52d576cdec0c487fcbf078af243938aa8
Rsingrin1 <rsingrin@csus.edu>
2025-12-01 10:37:54 -0800

    finished implementing the update user stuff

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,12 +1,14 @@
 import express from "express"
 
 //function to create new user
-import { create, getUserById, getAllUsers } from "../controller/userController.js"
+import { create, getUserById, getAllUsers, update} from "../controller/userController.js"
 
 const route = express.Router();
 
 route.post("/user",create);
 route.get("/user/:id",getUserById);
 route.get("/users", getAllUsers);
+route.put("/update/user/:id", update);
+
 
 export default route;
